1bf353fd33f9391aef91b94326a67ea4b1abbbc6
David Bremner <bremner@unb.ca>
2009-05-16 09:23:08 -0300

    remove example files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the bliss package
-#
-0 4	* * *	root	[ -x /usr/bin/bliss_maintenance ] && /usr/bin/bliss_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(bliss):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="bliss" command="/usr/bin/bliss"
